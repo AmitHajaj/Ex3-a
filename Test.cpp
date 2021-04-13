e57761878368448b059ff8b7890d1e661c7046df
‪Amit Hajaj‬‏ <57085762+hajajon@users.noreply.github.com>
2021-04-13 16:43:49 +0300

    Add files via upload

---
+++ Test.cpp
@@ -0,0 +1,78 @@
+#include "doctest.h"
+#include "NumberWithUnits.hpp"
+using namespace ariel;
+
+#include <iostream>
+#include <stdexcept>
+using namespace std;
+
+TEST_CASE("Good case for (+)"){
+
+}
+
+TEST_CASE("Bad case for (+)"){
+
+}
+TEST_CASE("Good case for (+=)"){
+
+}
+
+TEST_CASE("Bad case for (+=)"){
+
+}
+TEST_CASE("Good case for (-)"){
+
+}
+
+TEST_CASE("Bad case for (-)"){
+
+}
+TEST_CASE("Good case for (-=)"){
+
+}
+
+TEST_CASE("Bad case for (-=)"){
+
+}
+TEST_CASE("Good case for (-). Change sign"){
+
+}
+
+TEST_CASE("Bad case for (-). Change sign"){
+
+}
+TEST_CASE("Good case for comparisons"){
+
+}
+
+TEST_CASE("Bad case for comparisons"){
+
+}
+TEST_CASE("Good case for (++)"){
+
+}
+
+TEST_CASE("Bad case for (++)"){
+
+}
+TEST_CASE("Good case for (--)"){
+
+}
+
+TEST_CASE("Bad case for (--)"){
+
+}
+TEST_CASE("Good case for (*). left is number"){
+
+}
+
+TEST_CASE("Bad case for (*). left is number"){
+
+}
+TEST_CASE("Good case for (*). right is number"){
+
+}
+
+TEST_CASE("Bad case for (*). right is number"){
+
+}
